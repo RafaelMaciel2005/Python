0f8c5b1a2df4b0b9147eec14b76707170ad10a92
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-11-21 11:43:19 -0300

    Create desafio06.py

---
+++ Aula07/desafio06.py
@@ -0,0 +1,7 @@
+num = int(input("Digite seu número:"))
+dob = num*2
+trip = num*3
+raiz = num**(1/2)
+print("O dobro é: {}".format(dob)) 
+print("O triplo é: {}".format(trip)) 
+print("A raíz é: {}".format(raiz)) 
